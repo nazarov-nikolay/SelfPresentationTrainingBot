4e086744ee5e2d57c445620914800bd03f74a1a7
Nazarov Nikolai <nikolai.nazarov@simbirsoft.com>
2024-06-14 02:08:40 +0400

    fix sents.py

--- sents.py
+++ sents.py
@@ -1,7 +1,3 @@
-import io
-import spacy
-
-
 fillers = set([
     "знаешь",
     "знаете",
@@ -55,34 +51,3 @@ fillers = set([
     "то есть",
     "вообще"
 ])
-
-# nlp = spacy.load("ru_core_news_lg")
-
-# data = None
-# with io.open('artem_kazimir.txt','r', encoding='utf8') as f:
-#     data = f.read()
-
-# doc = nlp(data)
-
-# # with io.open('./result.txt','w', encoding='utf8') as f:
-# #     for sent in doc.sents:
-# #         f.write(str(sent) + '\n')
-
-# res = {}
-# data = data.lower()
-# for w in p:
-#    res[w] = data.count(w)
-
-# print(res)
-nlp = spacy.load("ru_core_news_lg")
-
-text = nlp('Ну типа привет как дела вообще ну как бы короче не знаю')#nlp(result["text"])
-words_count = 0
-for token in text:
-    if not token.is_punct and \
-       not token.is_space and \
-       not token.is_bracket and \
-       not token.is_quote:
-        words_count += 1
-
-print(words_count)
